0b336d70066cfc8b2640dc8714406d57af6b5c68
Venkatadri <villagetechvenkat@gmail.com>
2023-07-21 20:01:16 +0530

    added committee schema

---
+++ dto/committee.dto.js
@@ -0,0 +1,20 @@
+module.exports = {
+    name: {
+        notEmpty: true,
+    },
+    description: {
+        notEmpty: false
+    },
+    status: {
+        notEmpty: true
+    },
+    members: {
+        notEmpty: false
+    },
+    photos : {
+        notEmpty: false
+    },
+    funds : {
+        notEmpty: false
+    },
+}
